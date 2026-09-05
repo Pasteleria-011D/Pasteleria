bdcc4f61143078de12bf509cbc4f9fe80190440b
tigremago <ra.yanjari@duocuc.cl>
2026-09-05 18:42:27 -0400

    termino del js todavia no funciona con carrito

--- Frontend/js/catalogo.js
+++ Frontend/js/catalogo.js
@@ -1,11 +1,22 @@
 const botonesCarrito = document.querySelectorAll(".btn-carrito");
 
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
 botonesCarrito.forEach(function(boton){
-
     boton.addEventListener("click", function(){
+        const tarjeta = boton.closest(".prodctos-card");
+        const nombre = tarjeta.querySelector("h2").textContent;
+        const precioTexto = tarjeta.querySelector(".precio").textContent;
 
-        alert("Producto agregado al carrito");
+        const precio = parseInt(precioTexto.replace("$","").replace(".",""));
 
+        const producto = {
+            nombre : nombre,
+            precio : precio
+        };
+
+        carrito.push(producto);
+        localStorage.setItem("carrito",JSON.stringify(carrito));
+        alert("Producto agregado al carrito")
     });
-
-});
+})
