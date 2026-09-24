60ff58a73ace1f78efb0511643bb0f8f9ed0e99a
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-09-24 10:15:49 -0300

    subiendo de header y footer

--- Proyecto-React/src/componentes/Header.jsx
+++ Proyecto-React/src/componentes/Header.jsx
@@ -1,5 +1,24 @@
+import {Link} from "react-router-dom";
 function Header(){
-
+    return (
+        <header className="header">
+            <div className="title">
+                <span><h2>PASTELERIA MIL SABORES</h2></span>
+            </div>
+            <nav className="nav">
+                <Link to="/Index">Inicio</Link>
+                <Link to="/catalogo">Catalogo</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/nuestra-historia">Nuestra Historia</Link>
+                <Link to="/login">Inicio Sesion</Link>
+                <Link to="/registro">Registrate</Link>
+                <Link to="/contacto">Contactanos</Link>
+            </nav>
+            <div className="cart">
+                <a href="paginas/carrito.jsx"><img src="img/carrito.png"></img>Carrito</a>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
